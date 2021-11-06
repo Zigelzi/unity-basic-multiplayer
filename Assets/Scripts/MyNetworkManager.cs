f691da8c95e9a945e7aa87ee90ec7aedc33d26bd
Miika Savela <miika.a.savela@gmail.com>
2021-11-06 09:07:38 +0200

    Add basic custom NetworkManager and player prefab

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn) {
+        base.OnServerAddPlayer(conn);
+        Debug.Log("Player connected to server!");
+        Debug.Log($"Number of players: {numPlayers}");
+    }
+}
