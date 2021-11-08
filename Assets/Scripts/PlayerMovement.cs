656c2787da5375d6cc85248eea1c3d9097ef067f
Miika Savela <miika.a.savela@gmail.com>
2021-11-08 18:51:54 +0200

    Add player movement with NavMeshAgents and syncing it between clients and server

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    Camera playerCamera;
+    NavMeshAgent playerNavAgent;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        playerNavAgent = GetComponent<NavMeshAgent>();
+    }
+    [Command]
+    void CmdMove(Ray destination)
+    {
+        if (Physics.Raycast(destination, out RaycastHit hit, Mathf.Infinity))
+        {
+            playerNavAgent.SetDestination(hit.point);
+        }
+    }
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        base.OnStartAuthority();
+
+        playerCamera = Camera.main;
+        
+    }
+
+    [ClientCallback]
+    void Update()
+    {
+        if (!hasAuthority) { return; }
+
+        HandleMovementCommand();
+    }
+
+    void HandleMovementCommand()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray destination = playerCamera.ScreenPointToRay(Input.mousePosition);
+            CmdMove(destination);
+        }
+
+    }
+
+    
+
+    #endregion
+
+
+}
